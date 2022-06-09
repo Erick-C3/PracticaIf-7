daaeaf69a583fd8252d3ac5e9e43ecb99fa2697d
Erick <erick.nahuelpaiva.001@gmail.com>
2022-06-09 10:47:22 -0300

    modularizamos parte del programa

--- descuentos.js
+++ descuentos.js
@@ -12,40 +12,61 @@
 */
 
 const DESCUENTO_CARNE = 0.25;// 25%
-const DESCUENTO_LACTEO = 0.5; // 50%
+const DESCUENTO_LACTEO = 0.4; // 50%
 const DESCUENTO_ENLATADOS = 0.7; // 70%
+
+const BASE_PORCENTAJE = 100;
 
 const NOMBRE_PRODUCTO_CARNE = "carne";
 const NOMBRE_PRODUCTO_LACTEO = "lacteo";
 const NOMBRE_PRODUCTO_ENLATADO = "enlatado";
 
-let tipoProducto = "tipo de producto por def";
-let valorProducto = "precio por def";
+const DECIMALES_PRECIO_FINAL = 3;
 
-let precioFinal = "precio final por def";
+// vectores - arrays  // grafos // base de datos relacionales y no relaciones
+/* const MSJ_DESCUENTO_LACTEO = DESCUENTO_LACTEO*100 + "%"; // otra forma
+const MSJ_DESCUENTO_CARNE = "25%";
+const MSJ_DESCUENTO_ENLATADO = "70%"; */
+
+let tipoProducto = "tipo de producto por def";
+/* let valorProducto = "precio por def"; */
+
+/* let precioFinal = "precio final por def"; */
+
+
+function mostrarPrecioFinal(valorDescuento){
+    let valorProducto = document.querySelector("#precio_producto").value;
+    let precioFinal = valorProducto * (1 - valorDescuento);
+    document.querySelector("#precio_final").innerHTML = `
+    <h5>Precio final del producto: $${precioFinal.toFixed(DECIMALES_PRECIO_FINAL)}</h5>
+    <h6>Descuento aplicado: ${valorDescuento * BASE_PORCENTAJE} </h6>
+    `;
+}
 
 function calcularPrecioFinal(){
     tipoProducto = document.querySelector("#tipo_producto").value;
-    valorProducto = document.querySelector("#precio_producto").value;
-
+  
     if( tipoProducto == NOMBRE_PRODUCTO_LACTEO){
-        precioFinal = valorProducto * (1 - DESCUENTO_LACTEO);
+        mostrarPrecioFinal( DESCUENTO_LACTEO);//todo lo que esta entre parentesis es un argumento
+        /* precioFinal = valorProducto * (1 - DESCUENTO_LACTEO);
         document.querySelector("#precio_final").innerHTML = `
-        <h5>Precio final del producto: $${precioFinal}</h5>
+        <h5>Precio final del producto: $${precioFinal.toFixed(DECIMALES_PRECIO_FINAL)}</h5>
         <h6>Descuento aplicado: 50% </h6>
-        `;
+        `; */
     }else if(tipoProducto == NOMBRE_PRODUCTO_CARNE){
-        precioFinal = valorProducto * (1 - DESCUENTO_CARNE);
+        mostrarPrecioFinal( DESCUENTO_CARNE);
+        /* precioFinal = valorProducto * (1 - DESCUENTO_CARNE);
         document.querySelector("#precio_final").innerHTML = `
-        <h5>Precio final del producto: $${precioFinal}</h5>
+        <h5>Precio final del producto: $${precioFinal.toFixed(DECIMALES_PRECIO_FINAL)}</h5>
         <h6>Descuento aplicado: 25% </h6>
-        `;
+        `; */
     }else if(tipoProducto == NOMBRE_PRODUCTO_ENLATADO){
-        precioFinal = valorProducto * (1 - DESCUENTO_ENLATADOS);
+        mostrarPrecioFinal( DESCUENTO_ENLATADOS)
+        /* precioFinal = valorProducto * (1 - DESCUENTO_ENLATADOS);
         document.querySelector("#precio_final").innerHTML = `
-        <h5>Precio final del producto: $${precioFinal}</h5>
+        <h5>Precio final del producto: $${precioFinal.toFixed(DECIMALES_PRECIO_FINAL)}</h5>
         <h6>Descuento aplicado: 70% </h6>
-        `;
+        `; */
     }else{        
         document.querySelector("#precio_final").innerHTML = `
         <h5>Tipo de producto o precio ingresado incorrectos</h5>
